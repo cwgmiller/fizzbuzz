63de3b1467eb7a2fc4a161516b855d9e9905ce66
Robert Morgan <robbie.morgan@gmail.com>
2015-06-09 12:34:58 +0100

    Fizzbuzz complete

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,17 @@
+public
+
+class Integer < Numeric
+
+def fizzbuzz 
+	number =self.to_i
+  if number % 15 ==0
+  	'fizzbuzz'
+  elsif number % 3 ==0
+  	'fizz'
+  elsif number %5 ==0
+  	'buzz'
+  else
+  	number
+  end	
+end
+end
